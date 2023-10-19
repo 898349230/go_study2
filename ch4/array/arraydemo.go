c384022ac8f7678cff8596e45b988f81c6916360
sunxinbo <898349230@qq.com>
2023-10-19 23:43:53 +0800

    数组demo

--- ch4/array/arraydemo.go
+++ ch4/array/arraydemo.go
@@ -15,4 +15,15 @@ func main() {
 	arr3 := [3][2]int{{1, 2}, {2, 3}, {3, 4}}
 	fmt.Println(arr3)
 
+	arr4 := [...]int{6, 9, 0, 1}
+	fmt.Println(arr4)
+
+	for i := 0; i < len(arr4); i++ {
+		fmt.Println(i, " ", arr4[i])
+	}
+
+	for i, v := range arr4 {
+		fmt.Println(i, " ", v)
+	}
+
 }
